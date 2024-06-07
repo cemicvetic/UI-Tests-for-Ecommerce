ead20bc32ea8c7bafdb8eb60a3e09512d08ec214
cemicvetic <cemicvetic@gmail.com>
2024-06-07 14:17:04 -0700

    Update cart.cy.ts

--- cypress/e2e/cart.cy.ts
+++ cypress/e2e/cart.cy.ts
@@ -1,10 +1,9 @@
-import {LoginPage} from './login.page'
-import {InventoryPage } from './inventory.page'
-import {LoginInfo } from './index'
+import { LoginPage } from './login.page'
+import { InventoryPage } from './inventory.page'
+import {LoginInfo} from './index'
 
 describe('Cart', () => {
-  const user: LoginInfo = Cypress.env('user').standard
-
+  const user: LoginInfo = Cypress.env('users').standard
   if (!user) {
     throw new Error('Missing the standard user')
   }
@@ -14,13 +13,13 @@ describe('Cart', () => {
     cy.visit('/inventory.html')
     cy.location('pathname').should('equal', '/inventory.html')
   })
-  it('shows the added items in order they were added', {viewportHeight: 1200},
+  it('shows the added items in order they were added', { viewportHeight: 1200 },
     () => {
-    const items = [
-      'Sauce Labs Bike Light',
-      'Sauce Labs Bolt T-Shirt',
-      'Sauce Labs Onesie',
-    ]
+      const items = [
+        'Sauce Labs Bike Light',
+        'Sauce Labs Bolt T-Shirt',
+        'Sauce Labs Onesie',
+      ]
       items.forEach(InventoryPage.addItemToCart)
       cy.log('**added all items to cart**')
       InventoryPage.getCartBadge().should('have.text', items.length)
@@ -28,16 +27,24 @@ describe('Cart', () => {
         .wait(1000)
         .click()
       cy.location('pathname').should('equal', '/cart.html')
-      cy.get('.cart_list) .cart_item').should('have.length', items.length)
+      cy.get('.cart_list .cart_item').should('have.length', items.length)
       cy.log('**shows each items in order**')
-      items.forEach((itemName, q) => {
-        cy.get('.cart_list.cart_item').eq(q).within(() => {
-          cy.contains('.inventory_item_name', itemName)
-          cy.contains('.cart_quantity', 1)
-        })
-
+      items.forEach((itemName, k) => {
+        cy.get('.cart_list .cart_item')
+          .eq(k)
+          .within(() => {
+            cy.contains('.inventory_item_name', itemName)
+            cy.contains('.cart_quantity', 1)
+            cy.window()
+              .its('localStorage')
+              .invoke('getItem', 'cart-contents')
+              .should('exist')
+              // @ts-ignore
+              .then(JSON.parse)
+              .should('deep.equal', [0, 1, 2])
+          })
       })
-  })
+    })
 })
 
 
