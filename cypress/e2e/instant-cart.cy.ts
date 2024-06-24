3dbfaf3a9a8b5e52a4537a79fccf774449232b98
cemicvetic <cemicvetic@gmail.com>
2024-06-24 14:12:12 -0700

    Add instant-cart.cy.ts

---
+++ cypress/e2e/instant-cart.cy.ts
@@ -0,0 +1,35 @@
+import { LoginPage } from './login.page'
+import { LoginInfo } from '.'
+import {InventoryData} from '../../taste-the-sauce/src/utils/InventoryData'
+
+describe('Cart', () => {
+  const user: LoginInfo = Cypress.env('users').standard
+  if (!user) {
+    throw new Error('Missing the standard user')
+  }
+
+    beforeEach(() => {
+      LoginPage.login(user.username, user.password)
+    })
+
+    it('shows the cart items', { viewportHeight: 1200 }, () => {
+      const items = [
+        'Sauce Labs Bike Light',
+        'Sauce Labs Bolt T-Shirt',
+        'Sauce Labs Onesie',
+      ]
+      const ids = items.map((name) => Cypress._.find(InventoryData, { name })!.id)
+      window.localStorage.setItem('cart-contents', JSON.stringify(ids))
+      cy.visit('/cart.html')
+      cy.get('.cart_list .cart_item').should('have.length', items.length)
+      cy.log('**shows each item in order**')
+      items.forEach((itemName, k) => {
+        cy.get('.cart_list .cart_item')
+          .eq(k)
+          .within(() => {
+            cy.contains('.inventory_item_name', itemName)
+            cy.contains('.cart_quantity', 1)
+          })
+      })
+    })
+  })
