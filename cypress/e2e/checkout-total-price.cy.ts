77c5c750d92b9372bc3ce2371720b1bffbc5f8f8
cemicvetic <cemicvetic@gmail.com>
2024-07-02 11:32:07 -0700

    Add checkout.cy.ts

---
+++ cypress/e2e/checkout-total-price.cy.ts
@@ -0,0 +1,35 @@
+import { LoginPage } from './login.page'
+import { LoginInfo } from '.'
+import {InventoryData} from '../../taste-the-sauce/src/utils/InventoryData'
+
+describe('Checkout', () => {
+  const user: LoginInfo = Cypress.env('users').standard
+  if (!user) {
+    throw new Error('Missing the standard user')
+  }
+
+  beforeEach(() => {
+    LoginPage.login(user.username, user.password)
+  })
+
+  it('shows the right total price', { viewportHeight: 1200 }, () => {
+    const pickedItems = Cypress._.sampleSize(InventoryData, 3)
+    const ids = Cypress._.map(pickedItems, 'id')
+    window.localStorage.setItem('cart-contents', JSON.stringify(ids))
+    cy.visit('/checkout-step-one.html')
+    cy.get('.checkout_info_wrapper form').within(() => {
+      cy.get('#first-name').type('Lola')
+      cy.get('#last-name').type('Smith')
+      cy.get('#postal-code').type('90908')
+      cy.get('input[type=submit]')
+        .should('have.attr', 'value', 'Continue')
+        .click()
+    })
+    cy.location('pathname').should('equal', '/checkout-step-two.html')
+    cy.get('.cart_list .cart_item').should('have.length', pickedItems.length)
+    const prices = Cypress._.map(pickedItems, 'price')
+    const sum = Cypress._.sum(prices)
+    cy.log(prices.join(' + ') + ' = ' + sum)
+    cy.contains('.summary_subtotal_label', '$' + sum)
+  })
+})
