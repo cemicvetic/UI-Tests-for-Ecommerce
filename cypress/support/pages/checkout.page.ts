39d43e985b2d5cca42a5ed452ee4c2265333ee30
cemicvetic <cemicvetic@gmail.com>
2024-09-03 12:49:37 -0700

    Update tsconfig.json

--- cypress/support/pages/checkout.page.ts
+++ cypress/support/pages/checkout.page.ts
@@ -1,25 +1,11 @@
 export const CheckoutPage = {
-  /**
-   * Fills the checkout form with provided or default values.
-   * @param firstName First name, default "Lola"
-   * @param lastName Last name, default "Cen"
-   * @param zipCode Zip code, default "98524"
-   * @example
-   *  import { CheckoutPage } from './checkout.page'
-   *  CheckoutPage.fillInformationForm().submit()
-   * @example
-   *  // fill with your own information
-   *  CheckoutPage
-   *    .fillInformationForm('Mary', 'Brave', '01380')
-   *    .submit()
-   */
+
   fillInformationForm(
     firstName = 'Joe',
     lastName = 'Smith',
     zipCode = '90210',
   ) {
-    // make sure to return the command chain
-    // to allow chaining more assertions and commands
+
     return cy.get('.checkout_info_wrapper form').fillForm({
       '#first-name': firstName,
       '#last-name': lastName,
